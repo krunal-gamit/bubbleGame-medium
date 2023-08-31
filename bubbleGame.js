4806731d81e0862f68258a89463eb6fa9887b953
krunal-gamit <krooksgeek@gmail.com>
2023-08-31 23:52:56 +0530

    bubble game project done

--- bubbleGame.js
+++ bubbleGame.js
@@ -1,12 +1,20 @@
-let bubbleCount = "";
 let timer = 60;
 let hitVal;
+let score = 0;
+let flagValue = 0;
+
 function makeBubbles(){
-for (let i=0; i<152; i++){
-    let randNum = Math.floor(Math.random()*10);
-    bubbleCount += `<div class="bubble">${randNum}</div>`;
+    let bubbleCount = "";
+    for (let i=0; i<184; i++){
+        let randNum = Math.floor(Math.random()*10);
+        bubbleCount += `<div class="bubble">${randNum}</div>`;
+    }
+
+    document.querySelector('#bottomBar').innerHTML = bubbleCount;
 }
-document.querySelector('#bottomBar').innerHTML = bubbleCount;
+
+function clearBubbles(){
+    document.querySelector('#bottomBar').innerHTML = "";
 }
 
 function runTimer(){
@@ -16,6 +24,8 @@ function runTimer(){
         document.querySelector("#timerVal").innerHTML = timer;
        }
        else{
+        document.querySelector("#bottomBar").innerHTML = `<div class="dynamic"><h1>Game Over</h1><h2>Your Score: ${score}</h2></div>`;
+        flagCheck(1);
         clearInterval(clearTimer);
        }
     },1000);
@@ -23,29 +33,54 @@ function runTimer(){
 
 function hitUpdate(){
     hitVal = Math.floor(Math.random()*10);
-    document.querySelector("#hitVal").innerHTML = hitVal;
+    document.querySelector("#hitVal").textContent = hitVal;
 }
 
 function updateScore(){
-    let score = Number(document.querySelector("#scoreVal").textContent);
+    score = Number(document.querySelector("#scoreVal").textContent);
     score += 10;
-    document.querySelector("#scoreVal").innerHTML = score;
+    document.querySelector("#scoreVal").textContent = score;
 }
-
-makeBubbles();
-runTimer();
 
 document.querySelector("#bottomBar")
 .addEventListener("click",function(det){
     let clickVal = Number(det.target.textContent);
-    hitVal = Number(document.querySelector("#hitVal").textContent);        
-    if( clickVal === hitVal){
-
+    if(clickVal === hitVal){
         updateScore();
         hitUpdate();
         makeBubbles();
-        console.log("sucess");
     }
 })
+
+function start(){
+hitUpdate();
+makeBubbles();
+runTimer();
+}
+
+function flagCheck(fv){
+    console.log("hi");
+    if (fv === 0){
+        console.log("hi");
+        document.querySelector("#btn").textContent = "Retry";
+        document.querySelector("#mainMenu").style.display = "none";
+    }
+    else{
+        document.querySelector("#mainMenu").style.display = "block";
+        score = 0;
+        document.querySelector("#scoreVal").textContent = score;
+        document.querySelector("#hitVal").textContent = 0;
+        timer = 60;
+        document.querySelector("#timerVal").textContent = timer;
+
+    }
+}
+
+document.querySelector("#mainMenu").
+addEventListener("click", function(){
+    flagCheck(flagValue);
+    start();
+})
+
 
 
